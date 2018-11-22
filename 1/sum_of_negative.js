f81b712927f958a6f47abf9ccbee75e2a96443c9
Harrison Malone <harrison.malone2@gmail.com>
2018-11-22 22:35:10 +1100

    adding files

---
+++ 1/sum_of_negative.js
@@ -0,0 +1,8 @@
+function sumOfNegative(numbers) {
+
+}
+
+// Open the `02_sum_of_negative.js`. Implement the `sumOfNegative` function which takes one parameter `numbers` (of type `Array`) and returns a `Number`: the sum of negative numbers in the array. For example:
+
+// - sumOfNegatives([-1, 4, -2, 9, 18]) should return -3
+// - sumOfNegatives([15, 16, 17, 1000]) should return 0
