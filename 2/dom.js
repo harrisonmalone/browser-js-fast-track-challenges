7f19674a248db1c798a4754b0761dd792ee0b537
Harrison Malone <harrison.malone2@gmail.com>
2018-11-26 09:35:08 +1100

    added css class

--- 2/dom.js
+++ 2/dom.js
@@ -1,20 +1,21 @@
 // Here are some challenges. Solve them from top to bottom
   // Ex 1. Read the content of the email input
-
+  
   // Ex 2. Fill the content of the email input with your email
-
+  
   // Ex 3. Replace the email hint (next to the input) with 'This is my email now'
   // The text should be emphasized using a <strong> tag
-
+  
   // Ex 4. Add the .blue CSS class to the table header cells (th elements)
-
+  
   // Ex 5. Count the number of table body rows there are (team count!)
- 
+  
   // Ex 6. Say there is a 15th team added to the table.
   // Add a row at the bottom, this new team should have zero points.
-
+  
   // Ex 7. Write some code to sum all points given to all teams
-
+  
   // Ex 8. Change the background color of all table header cells to #DDF4FF
-
+  
   // Ex 9. Remove the "Email:" label from the DOM
+  
